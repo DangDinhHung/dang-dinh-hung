1b3523abca73ad1b5243f63d7c9aad4517f40c41
DangDinhHung <ddhung254@gmail.com>
2023-11-28 08:34:19 +0700

    fix header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,6 @@ module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
         extend: {
-            colors: {
-                'primary': '#21293D',
-            }
         },
     },
     plugins: [],

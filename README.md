cf7b1b20b0cf36d7d81697425b4b9a0b7e102560
DangDinhHung <ddhung254@gmail.com>
2023-12-10 22:12:47 +0700

    role folder

---
+++ README.md
@@ -0,0 +1,9 @@
+The role of the folder 7-1 parttern
+
+1. abstracts - ko nạp vào main.scss, cần ở đâu dùng ở đó
+2. base
+3. components
+4. layout
+5. pages
+6. themes
+7. vendors
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,22 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
+        screens: {
+            'xs': { 'min': '375px', 'max': '639px' },
+            'sm': { 'min': '640px', 'max': '767px' },
+            'md': { 'min': '768px', 'max': '1023px' },
+            'lg': { 'min': '1024px', 'max': '1279px' },
+            'xl': { 'min': '1280px', 'max': '1535px' },
+            '2xl': { 'min': '1536px' },
+        },
         extend: {
+            gridTemplateColumns: {
+                // Simple 16 column grid
+                '2': 'repeat(2, minmax(60px, 273px))',
+
+                'footer': '200px minmax(900px, 1fr) 100px',
+            }
         },
     },
     plugins: [],
